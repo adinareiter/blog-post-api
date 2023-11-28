e7adbc15abe0f72338b2654452109edc96044425
Derek McGee <dmcgee2001@gmail.com>
2023-11-28 14:00:11 -0500

    adds schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_183740) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
